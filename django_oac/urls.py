5effe9b7f5892bc968824a0cc61a23c244501fe9
Przemek Kaliś <przemek.kalis@gmail.com>
2020-08-20 14:38:58 +0200

    add views and urls

---
+++ django_oac/urls.py
@@ -0,0 +1,12 @@
+from django.urls import re_path
+
+from . import views
+
+
+app_name = "django_oac"
+urlpatterns = [
+    re_path(r"^authenticate/$", views.authenticate_view, name="authenticate"),
+    re_path(r"^callback/$", views.callback_view, name="callback"),
+    re_path(r"^logout/$", views.logout_view, name="logout"),
+    re_path(r"^test/$", views.test_view, name="test"),
+]
--- django_oac/views.py
+++ django_oac/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from uuid import uuid4
 
-# Create your views here.
+from django.conf import settings
+from django.contrib.auth import authenticate, login
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.utils import timezone
+
+
+def authenticate_view(request: WSGIRequest) -> HttpResponse:
+    state_str = uuid4().hex
+    request.session["OAC_STATE_STR"] = state_str
+    request.session["OAC_STATE_TIMESTAMP"] = timezone.now().timestamp()
+    return redirect(
+        f"{settings.OAC.get('authenticate_uri', '')}"
+        f"?scope={settings.OAC.get('scope', '')}"
+        f"&client_id={settings.OAC.get('client_id', '')}"
+        f"&redirect_uri={settings.OAC.get('redirect_uri', '')}"
+        f"&state={state_str}"
+        "&response_type=code"
+    )
+
+
+def callback_view(request: WSGIRequest) -> HttpResponse:
+    user = authenticate(request)
+    if user:
+        login(request, user, backend="django_oac.backends.OAuthClientBackend")
+    return redirect("django_oac:test")
+
+
+def logout_view(request: WSGIRequest) -> HttpResponse:
+    pass
+
+
+def test_view(request: WSGIRequest) -> HttpResponse:
+    pass
