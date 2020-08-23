9a7b8b5350e874636aaa7a265e7893d670ec5a74
Przemek Kaliś <przemek.kalis@gmail.com>
2020-08-23 23:12:48 +0200

    update

--- tests/test_callback_view.py
+++ tests/test_callback_view.py
@@ -3,29 +3,52 @@ from unittest.mock import patch
 
 import pytest
 from django.shortcuts import reverse
-from jwt.exceptions import ExpiredSignatureError, PyJWTError
+from jwt.exceptions import ExpiredSignatureError
 
-from django_oac.exceptions import OACError, ProviderResponseError
+from django_oac.apps import DjangoOACConfig
+from django_oac.exceptions import (
+    ExpiredStateError,
+    ProviderRequestError,
+    ProviderResponseError,
+)
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(DjangoOACConfig.name)
 
 
 @patch("django_oac.views.authenticate")
 @pytest.mark.parametrize(
-    "raised_exception,expected_message",
+    "exception,message,expected_message,expected_status_code",
     [
-        (PyJWTError, "foo"),
-        (ExpiredSignatureError, "bar"),
-        (OACError, "spam"),
-        (ProviderResponseError, "eggs"),
+        (ProviderRequestError, "foo", "raised ProviderRequestError: foo", 400),
+        (ExpiredStateError, "bar", None, 400),
+        (KeyError, "baz", "configuration error, missing 'baz'", 500),
+        (ProviderResponseError, "spam", "raised ProviderResponseError: spam", 500),
+        (ExpiredSignatureError, "eggs", "raised ExpiredSignatureError: eggs", 500),
     ],
 )
 def test_callback_view(
-    mock_authenticate, raised_exception, expected_message, client, caplog
+    mock_authenticate,
+    exception,
+    message,
+    expected_message,
+    expected_status_code,
+    client,
+    caplog,
 ):
-    mock_authenticate.side_effect = raised_exception(expected_message)
+    mock_authenticate.side_effect = exception(message)
 
-    client.get(reverse("django_oac:callback"))
+    response = client.get(reverse("django_oac:callback"))
 
-    for record in caplog.records:
-        assert record.msg == f"raised {raised_exception.__name__}: {expected_message}"
+    assert expected_status_code == response.status_code
+    assert expected_message == getattr(
+        next(
+            (
+                record
+                for record in caplog.records
+                if record.name == DjangoOACConfig.name
+            ),
+            None,
+        ),
+        "msg",
+        None,
+    )
