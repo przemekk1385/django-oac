d2a3bab8267952c5b2ba112bd277309d17106fcd
Przemek Kaliś <przemek.kalis@gmail.com>
2020-09-10 12:03:00 +0200

    remove hardcoded app name

--- django_oac/urls.py
+++ django_oac/urls.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
 
 from . import views
+from .apps import DjangoOACConfig
 
-app_name = "django_oac"
+app_name = DjangoOACConfig.name
 urlpatterns = [
     re_path(r"^authenticate/$", views.authenticate_view, name="authenticate"),
     re_path(r"^callback/$", views.callback_view, name="callback"),
